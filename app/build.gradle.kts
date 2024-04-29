c52bde12ce40c07f44c070171d04cc66aecbf7d0
antondulev <a.dulev@inpglobal.com>
2024-04-29 21:32:39 +0300

    adds gradle properties

---
+++ app/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("java")
+    id("checkstyle")
+    application
+}
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
