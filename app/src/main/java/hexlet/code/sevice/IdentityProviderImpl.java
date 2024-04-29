21081cba24a072a4d58d43255430ae87aa4e844c
antondulev <a.dulev@inpglobal.com>
2024-04-29 21:57:54 +0300

    makes final class

--- app/src/main/java/hexlet/code/sevice/IdentityProviderImpl.java
+++ app/src/main/java/hexlet/code/sevice/IdentityProviderImpl.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class IdentityProviderImpl implements IdentityProvider {
+public final class IdentityProviderImpl implements IdentityProvider {
 
     private List<Identity> registryList = new ArrayList<>();
 
